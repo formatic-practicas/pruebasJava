176306c4923491662f00861eb1b18d02590a1b7b
formatic-practicas <formatic@mundo-r.com>
2013-09-06 13:58:52 +0200

    muevo cosas

--- src/pruebasJava/VentanaAntonio.java
+++ src/pruebasJava/VentanaAntonio.java
@@ -1,14 +1,14 @@
 package pruebasJava;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JTextField;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class VentanaAntonio {
 
--- src/pruebasJava/ventanaManuela.java
+++ src/pruebasJava/ventanaManuela.java
@@ -1,17 +1,15 @@
 package pruebasJava;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.BorderLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class ventanaManuela {
 
